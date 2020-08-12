c4dbd021d793b33da37360ca2692e7569c9c72a4
Simon Hughesdon <simon.hughesdon@digital.cabinet-office.gov.uk>
2020-08-12 16:25:45 +0100

    Search filters

--- app/controllers/browse_controller.rb
+++ app/controllers/browse_controller.rb
@@ -53,7 +53,7 @@ class BrowseController < ApplicationController
       title: subtopic_details["title"],
       description: subtopic_details["description"],
       parent_url: "/browse/#{topic_slug}",
-      taxon_search_filter: taxon_filter(subtopic_slug),
+      taxon_search_filter: (taxon_filter_lookup(subtopic_slug) || ""),
       latest_news: {
         title: latest_news_content.first["title"],
         description: latest_news_content.first["description"],
@@ -209,6 +209,32 @@ private
       "asylum" => "08a8a69f-2825-4fe2-a4cf-c83458a5629e",
       "immigration-appeals" => "6e85c12f-f52b-41b3-93ad-59e5f19d64f6",
       "arriving-in-the-uk" => "ba3a9702-da22-487f-86c1-8334a730e559",
+    }
+  end
+
+  def taxon_filter_lookup
+    {
+      "benefits" => "level_one_taxon=dded88e2-f92e-424f-b73e-6ad24a839c51",
+      "entitlement" => "level_one_taxon=dded88e2-f92e-424f-b73e-6ad24a839c51&level_two_taxon=536f83c0-8c67-47a3-88a4-d5b1eda591ed",
+      "universal-credit" => "level_one_taxon=dded88e2-f92e-424f-b73e-6ad24a839c51&level_two_taxon=62fcbba5-3a75-4d15-85a6-d8a80b03d57c",
+      "tax-credits" => "level_one_taxon=dded88e2-f92e-424f-b73e-6ad24a839c51&level_two_taxon=a7f3005b-a3cd-4060-a127-725accb54f2e",
+      "jobseekers-allowance" => "level_one_taxon=dded88e2-f92e-424f-b73e-6ad24a839c51&level_two_taxon=2a1bd1b1-5025-4313-9e5b-8352dd46f1d6",
+      "disability" => "level_one_taxon=dded88e2-f92e-424f-b73e-6ad24a839c51&level_two_taxon=05a9527b-e6e9-4a68-8dd7-7d84e6a24eef",
+      "child" => "level_one_taxon=dded88e2-f92e-424f-b73e-6ad24a839c51&level_two_taxon=7a1ba896-b85a-4137-81d9-ab05b7ce67dd",
+      "families" => "level_one_taxon=dded88e2-f92e-424f-b73e-6ad24a839c51&level_two_taxon=29dbee2a-5865-489b-860f-7eef54a5165a",
+      "heating" => "level_one_taxon=dded88e2-f92e-424f-b73e-6ad24a839c51&level_two_taxon=6c4c443c-2e11-4d25-aa93-2e3a38d9499c",
+      "bereavement" => "level_one_taxon=dded88e2-f92e-424f-b73e-6ad24a839c51&level_two_taxon=ac7b8472-5d09-4679-9551-87847b0ac827",
+      "visas-immigration" => "level_one_taxon=ba3a9702-da22-487f-86c1-8334a730e559",
+      "what-you-need-to-do" => "level_one_taxon=ba3a9702-da22-487f-86c1-8334a730e559&level_two_taxon=6dc91505-fdbd-4b6b-9bd5-fef3dc8baf42",
+      "eu-eea-commonwealth" => "level_one_taxon=ba3a9702-da22-487f-86c1-8334a730e559&level_two_taxon=ba3a9702-da22-487f-86c1-8334a730e559",
+      "tourist-short-stay-visas" => "level_one_taxon=ba3a9702-da22-487f-86c1-8334a730e559&level_two_taxon=9480b00-dc4d-49a0-b48c-25dda8569325",
+      "student-visas" => "level_one_taxon=ba3a9702-da22-487f-86c1-8334a730e559&level_two_taxon=9480b00-dc4d-49a0-b48c-25dda8569325",
+      "work-visas" => "level_one_taxon=ba3a9702-da22-487f-86c1-8334a730e559&level_two_taxon=f48188df-8130-4d36-98e0-e72125d016a2",
+      "family-visas" => "level_one_taxon=ba3a9702-da22-487f-86c1-8334a730e559&level_two_taxon=9480b00-dc4d-49a0-b48c-25dda8569325",
+      "settle-in-the-uk" => "level_one_taxon=ba3a9702-da22-487f-86c1-8334a730e559&level_two_taxon=fef7e737-6f1a-4ef4-b844-aa24b630ad03",
+      "asylum" => "level_one_taxon=ba3a9702-da22-487f-86c1-8334a730e559&level_two_taxon=08a8a69f-2825-4fe2-a4cf-c83458a5629e",
+      "immigration-appeals" => "level_one_taxon=ba3a9702-da22-487f-86c1-8334a730e559&level_two_taxon=6e85c12f-f52b-41b3-93ad-59e5f19d64f6",
+      "arriving-in-the-uk" => "level_one_taxon=ba3a9702-da22-487f-86c1-8334a730e559&level_two_taxon=ba3a9702-da22-487f-86c1-8334a730e559",
     }
   end
 end
