767b72ac2e63bb1d440bdd41ffee63098ebebdd5
Max Froumentin <github@lapin-bl.eu>
2020-11-11 13:31:39 +0000

    Add a route for the list of topics

--- app/controllers/topic_controller.rb
+++ app/controllers/topic_controller.rb
@@ -26,6 +26,29 @@ class TopicController < ApplicationController
   end
 
 
+  def show_topics
+    browse_slug = params[:slug]
+    url = "https://www.gov.uk/api/content/topic"
+
+    content_item = http_get(url).parsed_response
+
+    subtopics = content_item["links"]["children"].sort_by { |k| k["title"] }
+
+
+    payload = {
+      title: content_item["title"],
+      description: content_item["description"],
+      subtopics: subtopics.map {
+        |subtopic| {
+          title: subtopic["title"],
+          link: subtopic["base_path"]
+        }
+      }
+    }
+
+    render json: payload
+  end
+
   # def subtopic
   #   payload = {"a": 2}
   #   render json: payload
